b97176a8b629ea848eb69c13b1e1a7b8ff5bca86
PARKHEECHANG <122577719+PARKHEECHANG@users.noreply.github.com>
2023-01-15 11:22:51 +0900

    Update Java.java

--- Language/Java.java
+++ Language/Java.java
@@ -3,6 +3,9 @@ for(int j=0; j<n; i++) // * j변수에 i++ 조심...
 Arrays.sort(arr, Comparator.comparingInt(o1 -> o1[0])); // * 이중배열 정렬
 
 // * 문자열 비교는 equals(), '==' 사용 시 주소값 비교.
+String str = "abc"
+// str.equals("abc") // O
+// str == "abc" // X
 
 Math.abs() // 절대값
 
