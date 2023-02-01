e711967694dd1e45364272c8754bf006cee35215
PARKHEECHANG <122577719+PARKHEECHANG@users.noreply.github.com>
2023-02-01 10:25:57 +0900

    Create 버블 정렬.py

---
+++ 버블 정렬.py
@@ -0,0 +1,12 @@
+# 버블 정렬
+lst = [55, 7, 78, 12, 42]
+
+for i in range(len(lst)-1, 0, -1) :
+    for j in range(i) :
+        if lst[j] > lst[j+1] :
+            tmp = lst[j]
+            lst [j] = lst[j+1]
+            lst[j+1] = tmp
+
+print(lst)
+# [7, 12, 42, 55, 78]
