05515c205105f396cf7709beed8710f0aca277bc
PARKHEECHANG <122577719+PARKHEECHANG@users.noreply.github.com>
2023-02-01 22:45:56 +0900

    Update 정렬.py

--- 정렬.py
+++ 정렬.py
@@ -4,20 +4,23 @@ for i in range(len(lst)-1, 0, -1) : # 각 구간의 끝 (1씩 감소)
     for j in range(i) : # 비교할 왼쪽 원소
         if lst[j] > lst[j+1] :
            lst[j], lst[j+1] = lst[j+1], lst[j] # 큰 원소 오른쪽으로
-
 print(lst)
 
 
 # 선택 정렬
 lst = list(map(int, input().split()))
-
 for i in range(len(lst)) :
     idx = i
     for j in range(i+1, len(lst)) :
         if lst[j] < lst[idx] :
             idx = j
     lst[idx], lst[i] = lst[i], lst[idx]
-
 print(lst)
 
-
+#삽입 정렬
+lst = list(map(int, input().split()))
+for i in range(1, len(lst)) :
+    for j in range(i, 0, -1) :
+        if lst[j-1] > lst[j] :
+            lst[j-1], lst[j] = lst[j], lst[j-1]
+print(lst)
