bbebbf7c2e32fd7f762716feb68b21c02c4fe512
PARKHEECHANG <122577719+PARKHEECHANG@users.noreply.github.com>
2023-02-01 10:37:07 +0900

    Update 버블 정렬.py

--- 버블 정렬.py
+++ 버블 정렬.py
@@ -2,7 +2,7 @@ lst = list(map(int, input().split())) # input : 55 7 78 12 42
 for i in range(len(lst)-1, 0, -1) : # 각 구간의 끝 (1씩 감소)
     for j in range(i) : # 비교할 왼쪽 원소
         if lst[j] > lst[j+1] :
-           lst[j], lst[j+1] = lst[j+1], lst[j]
+           lst[j], lst[j+1] = lst[j+1], lst[j] # 큰 원소 오른쪽으로
 
 print(lst)
 # [7, 12, 42, 55, 78]
